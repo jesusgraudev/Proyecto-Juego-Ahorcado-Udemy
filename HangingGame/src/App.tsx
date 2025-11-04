ab6733d1d37ee3fe355983087e5615a04221bc42
JesusGrauDev <jesusgrauortizz@gmail.com>
2025-11-03 19:04:36 -0500

    Seguimos trabajando en el proyecto se agregaron:
    - Imagenes del Juegos
    - Letras de opciones para elegir
    - Secciones que se usaran durante el juego.

--- HangingGame/src/App.tsx
+++ HangingGame/src/App.tsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { letters } from './helpers/letters'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      
+      {/* Imagenes  */}
+      <h3>Imagen del Juego</h3>
+
+
+      {/* Palabra  */}
+      <h3>_ _ _ _ _ _ _ _</h3>
+
+
+      {/* Intentos  */}
+      <h3>Intento: 0</h3>
+
+
+      {/* letras  */}
+      {
+        letters.map( (letter) =>  (
+          <button key={letter}>{letter}</button>
+        ))
+      }
+
+
+
+    </div>
   )
 }
 
